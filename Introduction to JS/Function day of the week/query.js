2101323785525c1d0fa62981750a0be6f1a101a8
Faith Mwale <patriciafaithmwale@gmail.com>
2023-02-03 19:51:22 +0200

    java

---
+++ Introduction to JS/Function day of the week/query.js
@@ -0,0 +1,36 @@
+let clickButton = document.getElementById("button");
+clickButton.addEventListener("click", function weekDay() {
+
+  let day = Number(document.getElementById("numberDay").value);
+
+  switch (day) {
+
+    case 0:
+    case 7:
+      alert("It's Sunday!")
+      break;
+    case 1:
+      alert("It's Monday");
+      break;
+    case 2:
+      alert("It's Tuesday");
+      break;
+    case 3:
+      alert("It's Wednesday");
+      break;
+    case 4:
+      alert("It's Thursday");
+      break;
+    case 5:
+      alert("It's Friday");
+      break;
+    case 6:
+      alert("It's Saturday");
+      break;
+    default: 
+      alert("Wrong input!");
+      break;
+
+    }
+} )
+
