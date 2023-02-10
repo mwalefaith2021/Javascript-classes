041559e4db47a233cdf31faf9844b33d45629f01
the_awesome_fayy <97280525+mwalefaith2021@users.noreply.github.com>
2023-02-10 17:19:00 +0200

    arrays foreach and push

---
+++ Introduction to JS/Arrays in js/index.js
@@ -0,0 +1,13 @@
+//foreach-executes a given function on every element on an array//
+const numbers = [1, 2, 3, 4, 5]
+
+
+//push//
+const numbers = [1, 2, 3]
+
+numbers.push(4);
+numbers.push(5);
+//const total = numbers.push(4,5,6,7,8);//
+//console.log(total)//
+
+console.log(numbers);
