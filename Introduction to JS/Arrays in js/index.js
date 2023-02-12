ce35235836575b1a1038ed6d4a259c77fd1addbb
Faith Mwale <patriciafaithmwale@gmail.com>
2023-02-12 16:11:20 +0200

    Update index.js

--- Introduction to JS/Arrays in js/index.js
+++ Introduction to JS/Arrays in js/index.js
@@ -1,13 +1,14 @@
 //foreach-executes a given function on every element on an array//
-const numbers = [1, 2, 3, 4, 5]
+let numbers = [1, 2, 3, 4, 5]
+
 
 
 //push//
-const numbers = [1, 2, 3]
+//const numbers = [1, 2, 3]h
 
-numbers.push(4);
-numbers.push(5);
-//const total = numbers.push(4,5,6,7,8);//
-//console.log(total)//
+//numbers.push(4);
+//numbers.push(5);
+//const total = numbers.push(4,5,6,7,8);
+//console.log(total)
 
-console.log(numbers);
+//console.log(numbers);
